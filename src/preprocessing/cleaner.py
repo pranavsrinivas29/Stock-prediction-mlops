d0fb883fae9fcf2da3485b5ed7b0e6a6cc54ff85
pranavsrinivasvenkatesh <pranav10srinivas@gmail.com>
2026-02-06 13:27:24 +0530

    Data cleaning methods

--- src/preprocessing/cleaner.py
+++ src/preprocessing/cleaner.py
@@ -4,10 +4,8 @@ import pandas as pd
 
 def clean_raw_stock_df(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
+    #print(df.columns)
     df["Date"] = pd.to_datetime(df["Date"])
-    df = df.set_index("Date")
-
-    # Sort for time-series correctness
-    df = df.sort_index()
+    df = df.set_index("Date").sort_index()
 
     return df
